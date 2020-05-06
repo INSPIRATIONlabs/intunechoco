e1aa87d7c6fc0018ba57cf112e8aeca3930acec4
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2020-05-06 15:35:56 +0200

    chore: fix prep tool error

--- run.ps1
+++ run.ps1
@@ -3,7 +3,8 @@ $dir | ForEach-Object {
     echo $_.fullname
     $fullpath = $_.fullname + "\install.ps1"
     $fullout = ".\output\" + $_.name
-    .\work\preptool\*\intunewinapputil.exe -c $_.FullName -s $fullpath -o $fullout -q
+    Start-Process -wait .\work\preptool\*\intunewinapputil.exe "-c `"$_.FullName`" -s `"$fullpath`" -o `"$fullout`" -q"
     $copyfiles = $_.FullName + "\*.ps1"
     Copy-Item -Path $copyfiles -Destination $fullout\
 }
+
